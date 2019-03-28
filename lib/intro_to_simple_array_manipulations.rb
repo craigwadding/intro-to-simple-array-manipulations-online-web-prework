bb0137862ee2da875ae7300c64e630e959bb848d
craig wadding <craig.wadding@gmail.com>
2019-03-28 00:57:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,1 +1,6 @@
 def using_push
+  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+    next_country = "Niger"
+    updated_array = using_push(countries_in_western_africa, next_country)
+    expect(updated_array.last).to eq(next_country)
+    end
