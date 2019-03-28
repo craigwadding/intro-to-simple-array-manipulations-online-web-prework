a43486b432b989886723b810249e1dabcb810c59
craig wadding <craig.wadding@gmail.com>
2019-03-28 01:02:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,4 @@
 
-  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-    next_country = "Niger"
-    countries_in_western_africa.push(next_country)
+countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+next_country = "Niger"
+countries_in_western_africa.push(next_country)
